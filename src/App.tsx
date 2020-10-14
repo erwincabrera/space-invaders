d751f4e6a5eeb506283a6a0dba15b95af237dded
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-15 02:07:28 +0800

    Add movement controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Graph from './Graph';
 
+const INTERVAL_MS = 50;
+const X0 = 100;
+const Y0 = 100;
+const DX = 20;
+const DY = 20;
+const MOVEMENT = {
+  UP: 'w',
+  DOWN: 's',
+  LEFT: 'a',
+  RIGHT: 'd'
+}
+const isKeyDown = {}
+
 const App = () => {
+  
+  const [x, setX] = useState(X0);
+  const [y, setY] = useState(Y0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (isKeyDown[MOVEMENT.UP]) setY(y => y - DY)
+      if (isKeyDown[MOVEMENT.DOWN]) setY(y => y + DY)
+      if (isKeyDown[MOVEMENT.LEFT]) setX(x => x - DX)
+      if (isKeyDown[MOVEMENT.RIGHT]) setX(x => x + DX)
+    }, INTERVAL_MS)
+    
+    const handleKeyup = (e: KeyboardEvent) => {
+      if (Object.values(MOVEMENT).indexOf(e.key) !== -1) {
+        isKeyDown[e.key] = false
+      }
+    }
+    
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (Object.values(MOVEMENT).indexOf(e.key) !== -1) {
+        isKeyDown[e.key] = true
+      }
+    }
+
+    window.addEventListener('keydown', handleKeydown);
+    window.addEventListener('keyup', handleKeyup);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+      window.removeEventListener('keyup', handleKeyup);
+      clearInterval(intervalId);
+    }
+  }, [])
+
+
   return (
-    <Graph></Graph>
+    <div>
+      <Graph x={x} y={y}></Graph>
+    </div>
   )
 }
 
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -12,7 +12,12 @@ const mxClient = myGraph.mxClient;
 const mxUtils = myGraph.mxUtils;
 const mxEvent = myGraph.mxEvent;
 
-class Graph extends Component {
+interface Props {
+  x: number;
+  y: number
+}
+
+class Graph extends Component<Props> {
   constructor(props: any) {
     super(props);
     this.state = {};
@@ -23,19 +28,25 @@ class Graph extends Component {
     this.LoadGraph();
   }
 
+  componentDidUpdate() {
+    this.LoadGraph();
+  }
+
   LoadGraph() {
-    var container = ReactDOM.findDOMNode(this.refs.divGraph);
+    const container = ReactDOM.findDOMNode(this.refs.divGraph) as HTMLElement;
 
     // Checks if the browser is supported
     if (!mxClient.isBrowserSupported()) {
       // Displays an error message if the browser is not supported.
       mxUtils.error("Browser is not supported!", 200, false);
     } else {
+      container.innerHTML = '';
+
       // Disables the built-in context menu
       mxEvent.disableContextMenu(container);
 
       // Creates the graph inside the given container
-      var graph = new mxGraph(container);
+      const graph = new mxGraph(container);
 
       // Gets the default parent for inserting new cells. This is normally the first
       // child of the root (ie. layer 0).
@@ -45,15 +56,14 @@ class Graph extends Component {
 
       graph.getModel().beginUpdate();
       try {
-        var v1 = graph.insertVertex(parent, null, "Hello,", 20, 20, 80, 30);
-        var v2 = graph.insertVertex(parent, null, "World!", 200, 150, 80, 30);
-        graph.insertEdge(parent, null, "", v1, v2);
+        graph.insertVertex(parent, null, "USS\nEnterprise", this.props.x, this.props.y, 75, 75);
       } finally {
         // Updates the display
         graph.getModel().endUpdate();
       }
     }
   }
+
   render() {
     return <div className="graph-container" ref="divGraph" id="divGraph" />;
   }
