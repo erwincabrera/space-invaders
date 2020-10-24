0ba058359d99bc9df8d8c4d202ffdf19c9e3f467
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-24 14:49:03 +0800

    Fix hit logic

--- src/App.tsx
+++ src/App.tsx
@@ -103,9 +103,10 @@ const reducer = (state: State, action: Action<any>): State => {
             }
           }))
           .filter(eachInvader => eachInvader.pos.y < 600)
-          .filter(eachInvader => 
-            state.shots.map(eachShot => eachShot.x).indexOf(eachInvader.pos.x) === -1 &&
-            state.shots.map(eachShot => eachShot.y).indexOf(eachInvader.pos.y) === -1
+          .filter(eachInvader => !state.shots.some(eachShot => 
+              Math.abs(eachShot.x - eachInvader.pos.x) < 50 && 
+              Math.abs(eachShot.y - eachInvader.pos.y) < 50
+            )
           )
       }
     case 'CREATE_INVADER':
