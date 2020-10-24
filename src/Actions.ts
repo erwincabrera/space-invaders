0369f40f93a47ade5038f44d144e5932311451ef
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-24 15:58:52 +0800

    Formatting changes to actions

--- src/Actions.ts
+++ src/Actions.ts
@@ -1,30 +1,63 @@
 import { Action, MovePayload, Position } from "./types";
 import * as Constants from "./Constants";
 
-export const moveUp = (): Action<MovePayload> => ({
-  type: 'MOVE', payload: { dx: 0, dy: -Constants.DY }
-});
+export const moveUp = (): Action<MovePayload> => {
+  return {
+    type: 'MOVE',
+    payload: {
+      dx: 0,
+      dy: -Constants.DY
+    }
+  }
+}
 
-export const moveDown = (): Action<MovePayload> => ({
-  type: 'MOVE', payload: { dx: 0, dy: Constants.DY }
-});
+export const moveDown = (): Action<MovePayload> => {
+  return {
+    type: 'MOVE',
+    payload: {
+      dx: 0,
+      dy: Constants.DY
+    }
+  }
+}
 
-export const moveLeft = (): Action<MovePayload> => ({
-  type: 'MOVE', payload: { dx: -Constants.DX, dy: 0 }
-});
+export const moveLeft = (): Action<MovePayload> => {
+  return {
+    type: 'MOVE',
+    payload: {
+      dx: -Constants.DX,
+      dy: 0
+    }
+  }
+}
 
-export const moveRight = (): Action<MovePayload> => ({
-  type: 'MOVE', payload: { dx: Constants.DX, dy: 0 }
-})
+export const moveRight = (): Action<MovePayload> => {
+  return {
+    type: 'MOVE',
+    payload: {
+      dx: Constants.DX,
+      dy: 0
+    }
+  }
+}
 
-export const fire = (): Action<any> => ({
-  type: 'FIRE', payload: {}
-})
+export const fire = (): Action<any> => {
+  return {
+    type: 'FIRE',
+    payload: {}
+  }
+}
 
-export const tick = (): Action<any> => ({
-  type: 'TICK', payload: {}
-})
+export const tick = (): Action<any> => {
+  return {
+    type: 'TICK',
+    payload: {}
+  }
+}
 
-export const createInvader = (pos: Position): Action<Position> => ({
-  type: 'CREATE_INVADER', payload: pos
-})
+export const createInvader = (pos: Position): Action<Position> => {
+  return {
+    type: 'CREATE_INVADER',
+    payload: pos
+  }
+}
