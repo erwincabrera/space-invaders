b245c7298222783204f056702eda85debf18c59e
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-29 00:43:29 +0800

    Add test for impact considering height

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -76,3 +76,113 @@ test('destroys both shot and invader upon impact', () => {
 
   expect(reducer(state, Actions.tick())).toEqual(expected)
 })
+
+test('destroys both shot and invader upon impact considering heights - just touching', () => {
+  const invaderHeight = 20;
+  const state: State = {
+    invaders: [
+      {
+        height: invaderHeight,
+        width: 20,
+        pos: { x: 50, y: 500 }
+      }
+    ],
+    shots: [
+      { x: 50, y: 500 + invaderHeight }
+    ],
+    player: {
+      height: 50,
+      width: 50,
+      pos: { x: 50, y: 1000 },
+      cooldown: 10,
+    }
+  };
+
+  const expected: State = {
+    ...state,
+    invaders: [],
+    shots: [],
+    player: {
+      ...state.player,
+      cooldown: 9
+    }
+  }
+
+  expect(reducer(state, Actions.tick())).toEqual(expected)
+})
+
+test('destroys both shot and invader upon impact considering heights - max distance', () => {
+  const invaderHeight = 20;
+  const state: State = {
+    invaders: [
+      {
+        height: invaderHeight,
+        width: 20,
+        pos: { x: 50, y: 500 + Constants.INVADER_DY}
+      }
+    ],
+    shots: [
+      { x: 50, y: 500 + invaderHeight - Constants.SHOT_DY}
+    ],
+    player: {
+      height: 50,
+      width: 50,
+      pos: { x: 50, y: 1000 },
+      cooldown: 10,
+    }
+  };
+
+  const expected: State = {
+    ...state,
+    invaders: [],
+    shots: [],
+    player: {
+      ...state.player,
+      cooldown: 9
+    }
+  }
+
+  expect(reducer(state, Actions.tick())).toEqual(expected)
+})
+
+test('destroys both shot and invader upon impact considering heights - distance too large', () => {
+  const invaderHeight = 20;
+  const state: State = {
+    invaders: [
+      {
+        height: invaderHeight,
+        width: 20,
+        pos: { x: 50, y: 500 + Constants.INVADER_DY + 0.5}
+      }
+    ],
+    shots: [
+      { x: 50, y: 500 + invaderHeight - Constants.SHOT_DY}
+    ],
+    player: {
+      height: 50,
+      width: 50,
+      pos: { x: 50, y: 1000 },
+      cooldown: 10,
+    }
+  };
+
+  const expected: State = {
+    ...state,
+    invaders: [
+      {
+        height: invaderHeight,
+        width: 20,
+        pos: { x: 50, y: 500 + 2*Constants.INVADER_DY + 0.5}
+      }
+    ],
+    shots: [
+      { x: 50, y: 500 + invaderHeight - 2*Constants.SHOT_DY}
+    ],
+    player: {
+      ...state.player,
+      cooldown: 9
+    }
+  }
+
+  expect(reducer(state, Actions.tick())).toEqual(expected)
+})
