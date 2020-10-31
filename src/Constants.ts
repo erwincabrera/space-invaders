ae9d6bfb3b908b011bbdaabce55a7378f904c0ed
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-31 12:28:08 +0800

    Smoother movement

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,12 +1,14 @@
+export const FPS = 60;
+export const SHOTS_PER_SEC = 3;
 export const HEIGHT = 600;
 export const WIDTH = 1024;
-export const TICK_MS = 60;
-export const INTERVAL_MS = 60;
-export const SHOT_COOLDOWN = 10;
-export const DX = 20;
-export const DY = 20;
-export const SHOT_DY = 100;
-export const INVADER_DY = 10;
+export const TICK_MS = 1000 / FPS;
+export const INTERVAL_MS = TICK_MS;
+export const SHOT_COOLDOWN = FPS / SHOTS_PER_SEC;
+export const DX = 400 / FPS;
+export const DY = 400 / FPS;
+export const SHOT_DY = 20;
+export const INVADER_DY = 2;
 export const SHOT_WIDTH = 10;
 export const SHOT_HEIGHT = 10;
 
