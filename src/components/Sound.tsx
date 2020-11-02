c182d2bb04798d06f8fad7418bbeb7fad47dda75
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-03 00:13:37 +0800

    Add component for playing sound

---
+++ src/components/Sound.tsx
@@ -0,0 +1,27 @@
+import React, { useImperativeHandle } from 'react'
+
+export interface SoundProps {
+  audio: any;
+}
+
+export interface SoundRef {
+  play: () => void;
+}
+
+export const Sound = React.forwardRef<SoundRef, SoundProps>((props, ref) => {
+  const audioPtRef = React.useRef<HTMLAudioElement>()
+
+  useImperativeHandle(ref, () => {
+    return {
+      play: () => {
+        audioPtRef.current.currentTime = 0;
+        audioPtRef.current.play();
+      }
+    }
+  })
+
+
+  return (
+    <audio ref={audioPtRef} src={props.audio} />
+  )
+})
