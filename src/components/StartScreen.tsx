891b9ac4ecf693532c24bd6ef05c61a9ad5e43f7
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-04 23:12:51 +0800

    Start screen styling complete

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-export const StartScreen = ({handleStart}) => {
+export const StartScreen = ({ handleStart }) => {
   return (
     <div className='start-screen'>
-      <button onClick={handleStart}>Start</button>
+      <div className='start-text'>
+        <section className='controls'>
+          <h1>Controls</h1>
+          <ul>
+            <li>
+              <p className='movement-key'>W</p>
+              <p>Move up</p>
+            </li>
+            <li>
+              <p className='movement-key'>S</p>
+              <p>Move down</p>
+            </li>
+            <li>
+              <p className='movement-key'>A</p>
+              <p>Move left</p>
+            </li>
+            <li>
+              <p className='movement-key'>D</p>
+              <p>Move right</p>
+            </li>
+            <li>
+              <p className='movement-key'>Spacebar</p>
+              <p>Fire photon torpedos</p>
+            </li>
+          </ul>
+        </section>
+        <button onClick={handleStart}>Start</button>
+      </div>
     </div>
   )
 }
