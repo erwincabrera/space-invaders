77823c3640551a0c1468589fc502f8989b257d58
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-06 00:47:19 +0800

    Move helpers to helper file

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,19 @@
-import { Position } from "./types";
+import * as Constants from "./Constants";
+import { Invader, Position } from "./types";
+
+export const isHit = (invader: Invader, shot: Position): boolean => {
+  return shot.x - invader.geo.pos.x <= invader.geo.width &&
+    shot.x - invader.geo.pos.x >= -Constants.SHOT_WIDTH &&
+    shot.y - invader.geo.pos.y <= invader.geo.height &&
+    shot.y - invader.geo.pos.y >= -(Constants.SHOT_DY - invader.geo.height + Constants.INVADER_DY)
+}
+
+export const isWithinBounds = (pos: Position, width: number, height: number, maxWidth: number, maxHeight: number): boolean => {
+  return pos.y + height < maxHeight &&
+    pos.y > 0 &&
+    pos.x + width < maxWidth &&
+    pos.x > 0;
+}
 
 export const dx = (p1: Position, p2: Position): number => {
   return  p2.x - p1.x;
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,5 +1,6 @@
 import { Action, Invader, MovePayload, State, Position } from "./types"
 import * as Constants from './Constants'
+import { isHit, isWithinBounds } from "./helpers";
 
 export const initialState: State = {
   isStarted: false,
@@ -133,18 +134,4 @@ export const reducer = (state: State, action: Action<any>): State => {
     default:
       return initialState;
   }
-}
-
-const isHit = (invader: Invader, shot: Position): boolean => {
-  return shot.x - invader.geo.pos.x <= invader.geo.width &&
-    shot.x - invader.geo.pos.x >= -Constants.SHOT_WIDTH &&
-    shot.y - invader.geo.pos.y <= invader.geo.height &&
-    shot.y - invader.geo.pos.y >= -(Constants.SHOT_DY - invader.geo.height + Constants.INVADER_DY)
-}
-
-const isWithinBounds = (pos: Position, width: number, height: number, maxWidth: number, maxHeight: number): boolean => {
-  return pos.y + height < maxHeight &&
-          pos.y > 0 &&
-          pos.x + width < maxWidth &&
-          pos.x > 0;
 }
