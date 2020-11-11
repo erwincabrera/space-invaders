6b53c2a250e403a9b24282fb158d99459e3073e3
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-11 20:45:22 +0800

    Refactoring start screen styling

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -2,35 +2,33 @@ import React from 'react'
 
 export const StartScreen = ({ handleStart }) => {
   return (
-    <div className='start-screen-container'>
-      <div className='start-screen'>
-        <section className='controls'>
-          <h1>Controls</h1>
-          <ul>
-            <li>
-              <p className='movement-key'>W</p>
-              <p>Move up</p>
-            </li>
-            <li>
-              <p className='movement-key'>S</p>
-              <p>Move down</p>
-            </li>
-            <li>
-              <p className='movement-key'>A</p>
-              <p>Move left</p>
-            </li>
-            <li>
-              <p className='movement-key'>D</p>
-              <p>Move right</p>
-            </li>
-            <li>
-              <p className='movement-key'>Spacebar</p>
-              <p>Fire photon torpedos</p>
-            </li>
-          </ul>
-        </section>
+    <div className='start-screen'>
+      <section className='controls'>
+        <h1>Controls</h1>
+        <ul>
+          <li>
+            <p className='movement-key'>W</p>
+            <p>Move up</p>
+          </li>
+          <li>
+            <p className='movement-key'>S</p>
+            <p>Move down</p>
+          </li>
+          <li>
+            <p className='movement-key'>A</p>
+            <p>Move left</p>
+          </li>
+          <li>
+            <p className='movement-key'>D</p>
+            <p>Move right</p>
+          </li>
+          <li>
+            <p className='movement-key'>Spacebar</p>
+            <p>Fire photon torpedos</p>
+          </li>
+        </ul>
         <button onClick={handleStart}>Start</button>
-      </div>
+      </section>
     </div>
   )
 }
